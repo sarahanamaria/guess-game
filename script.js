fc1cebc3337f79bae437f92ddf18e97bc4a438a2
sarahanamaria <110899240+sarahanamaria@users.noreply.github.com>
2022-09-24 11:18:53 +0200

    added features

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // when there's no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number';
+
+    // when the player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // when the guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+    } else {
+      document.querySelector('.message').textContent = '🧨 You lost the game!';
+    }
+    document.querySelector('.score').textContent = score;
+
+    // when the guess is too low
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
